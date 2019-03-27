926ad8ec91e0a35f205394245216d1a7b258bdf1
Cosmin Seceleanu <coosmin26@yahoo.com>
2019-03-27 19:43:15 +0200

    add base project

---
+++ source/src/main/java/com/cosmin/reactive/demo/source/ReactiveApplication.java
@@ -0,0 +1,11 @@
+package com.cosmin.reactive.demo.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+}
